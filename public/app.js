cbd813d1811a769bb3e6c8e40f8ec6e017c994ed
Jon Dotsoy <hi@jon.soy>
2015-12-14 22:30:49 -0300

    step 5

--- public/app.js
+++ public/app.js
@@ -2,5 +2,15 @@ System.config({
 });
 
 System.import("/react/calculator.js").then(function (calculator) {
+	System.import("react").then(function (React) {
+		System.import("react-dom").then(function (ReactDOM) {
+			window.ReactDOM = ReactDOM;
+			window.app = calculator;
+			window.React = React;
+			console.log("OK Load");
+
+			ReactDOM.render(React.createElement(calculator, null), document.getElementById("app-calculator"));
+		})
+	});
 });
 
